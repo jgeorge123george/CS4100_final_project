68cd138b54aeb84b84d43167ee00fcbb5ecde8e4
Tammer Haddad <tammerrh@gmail.com>
2025-11-26 14:46:15 -0500

    model abstraction and EMNIST data

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,6 +2,6 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
 
-def get_data_loader(batch_size):
+def get_data_loader(batch_size=32):
     train_data = datasets.MNIST(root="data", train=True, download=True, transform=ToTensor())
-    return DataLoader(train_data, batch_size)
+    return DataLoader(train_data, batch_size=batch_size)
--- src/model.py
+++ src/model.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 
 class ImageClassifier(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes=10):
         super().__init__()
         self.model = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=3), 
@@ -12,7 +12,7 @@ class ImageClassifier(nn.Module):
             nn.Conv2d(64, 64, kernel_size=3), 
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(64 * 22 * 22, 10)       
+            nn.Linear(64 * 22 * 22, num_classes)       
         )
 
     def forward(self, x):
